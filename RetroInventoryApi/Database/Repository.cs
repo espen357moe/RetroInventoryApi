ac929f748bf338ef401af07be0cc3dff1b3102ce
Espen Moe <espen357moe@gmail.com>
2024-03-03 00:11:09 +0100

    Removed an unused dependency, removed the MockCreator class

--- RetroInventoryApi/Database/Repository.cs
+++ RetroInventoryApi/Database/Repository.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using RetroInventoryApi.Domain;
 
 namespace RetroInventoryApi.Database
