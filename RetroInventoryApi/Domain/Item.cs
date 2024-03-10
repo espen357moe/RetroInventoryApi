192f4693c27d574e58412e6cb9f6c73123b5e910
Espen Moe <espen357moe@gmail.com>
2024-03-10 13:17:22 +0100

    Added description field

--- RetroInventoryApi/Domain/Item.cs
+++ RetroInventoryApi/Domain/Item.cs
@@ -6,6 +6,7 @@
         public string? Manufacturer { get; set; }
         public string? Model { get; set; }
         public string? SerialNumber { get; set; }
+        public string? Description { get; set; }
         public string? Notes { get; set; }
         public string? Location { get; set; }
         public CosmeticCondition CosmeticCondition { get; set; }
